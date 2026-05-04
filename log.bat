94aebf12573bf9f89a92aadc788f37d9ea3956d6
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-05-04 12:16:17 +0430

    i commit the code but not show the commit it is scure  1

--- log.bat
+++ log.bat
@@ -4,7 +4,7 @@ cd /d C:\your-project-path
 for /l %%x in (1, 1, 10) do (
     echo Update %%x >> log.txt
     git add .
-    git commit -m "Auto commit %%x"
+    git commit -m "i commit the code but not show the commit it is scure  %%x"
 )
 
 git push origin main

5fa25036773b9e21b937f238bd95b2fe7b2e9556
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-05-10 11:37:47 +0430

    Add commit  7

--- log.bat
+++ log.bat
@@ -36,3 +36,4 @@ i commit the code but not show the commit it is scure 3
 i commit the code but not show the commit it is scure 4 
 i commit the code but not show the commit it is scure 5 
 i commit the code but not show the commit it is scure 6 
+i commit the code but not show the commit it is scure 7 
